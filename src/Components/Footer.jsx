6cf023f49454ea5a6ee67e2317e3670d561184bb
Utkarsh Katiyar <UKatiyar@initializ.io>
2024-06-10 12:31:23 +0530

    The Great Commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+    © 2024, Made with
+❤️
+by
+<a href='https://www.initializ.ai/'> initializ inc.</a>
+    </p>
+  </div>
+);
+
+export default Footer;
